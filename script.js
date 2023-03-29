142afeaa5ca37cc8d0ffd30ca30f005c5966c5d1
Michael Eisenbraun <eisenbm@algonquincollege.com>
2023-03-29 11:05:58 -0400

    Creates final version

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
 const $books = document.querySelector('#books')
 const $modal = document.querySelector('.modal')
+const readBooks = JSON.parse(window.localStorage.getItem('readBooks')) || []
 
 function createBookTitle (title) {
   const $bookTitle = document.createElement('h2')
@@ -15,9 +16,14 @@ function createBookTitle (title) {
   return $bookTitle
 }
 
-function createBookButton () {
+function createBookButton (title) {
   const $readButton = document.createElement('button')
   $readButton.classList.add('book-button')
+
+  if (readBooks.includes(title)) {
+    console.log(title)
+    $readButton.classList.add('read')
+  }
 
   return $readButton
 }
@@ -26,7 +32,11 @@ function createBook (book) {
   const $bookContainer = document.createElement('div')
   $bookContainer.classList.add('book')
 
-  $bookContainer.append(createBookTitle(book.title), createBookButton())
+  $bookContainer.dataset.title = book.title
+  $bookContainer.dataset.description = book.description
+  $bookContainer.dataset.image = book.image
+
+  $bookContainer.append(createBookTitle(book.title), createBookButton(book.title))
   return $bookContainer
 }
 
@@ -37,3 +47,98 @@ function renderBooks (books) {
     $bookContainer.append(createBook(book))
   })
 }
+
+async function fetchBooks () {
+  const books = JSON.parse(window.localStorage.getItem('books'))
+
+  if (!books) {
+    const response = await fetch('https://seussology.info/api/books')
+    const json = await response.json()
+    window.localStorage.setItem('books', JSON.stringify(json))
+
+    renderBooks(json)
+  }
+
+  renderBooks(books)
+}
+
+fetchBooks()
+
+function createBookCover (imageURL) {
+  const $bookCover = document.createElement('img')
+  $bookCover.classList.add('book-cover')
+  $bookCover.src = imageURL
+  return $bookCover
+}
+
+function createModalContentTitle (title) {
+  const $modalContentTitle = document.createElement('h3')
+  $modalContentTitle.classList.add('modal-content-title')
+  $modalContentTitle.textContent = title
+  return $modalContentTitle
+}
+
+function createModalContentDescription (description) {
+  const $modalContentDescription = document.createElement('p')
+  $modalContentDescription.classList.add('modal-content-description')
+  $modalContentDescription.textContent = description
+  return $modalContentDescription
+}
+
+function createModalDetails (title, description) {
+  const $modalDetails = document.createElement('div')
+  $modalDetails.classList.add('modal-details')
+  $modalDetails.append(
+    createModalContentTitle(title),
+    createModalContentDescription(description)
+  )
+
+  return $modalDetails
+}
+
+function createModalContent (book) {
+  const $modalContent = document.createElement('div')
+  $modalContent.classList.add('modal-content')
+  $modalContent.append(
+    createBookCover(book.image),
+    createModalDetails(book.title, book.description)
+  )
+
+  return $modalContent
+}
+
+function addOrRemoveBook (title) {
+  if (readBooks.includes(title)) {
+    const index = readBooks.indexOf(title)
+    readBooks.splice(index, 1)
+  } else {
+    readBooks.push(title)
+  }
+
+  console.log(readBooks)
+}
+
+$books.addEventListener('click', function (event) {
+  const book = event.target.closest('.book')
+  if (book) {
+    const title = book.dataset.title
+
+    console.log(event.target)
+
+    if (event.target.matches('.book-button')) {
+      event.target.classList.toggle('read')
+      addOrRemoveBook(title)
+      fetchBooks()
+    } else {
+      $modal.innerHTML = ''
+      $modal.appendChild(createModalContent(book.dataset))
+      $modal.classList.add('show')
+    }
+  }
+})
+
+$modal.addEventListener('click', function (event) {
+  if (event.target === $modal) {
+    $modal.classList.remove('show')
+  }
+})
